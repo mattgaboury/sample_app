146e893a209ac0fa6810d61b8915acb9ca8b96fc
mattgaboury <mattgaboury@msn.com>
2011-03-17 14:31:15 -0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,6 +15,18 @@ describe PagesController do
     end
   end
   
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+      
+     it "should have the right title" do
+       get 'help'
+       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+  
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
