8a2ead17fba33478c050ab854906af4fea687ced
mattgaboury <mattgaboury@msn.com>
2011-03-22 00:00:46 -0400

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
   match '/signup',  :to => 'users#new'
   
   root :to => 'pages#home'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
